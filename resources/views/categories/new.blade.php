3e7b49f9db73fea70a7f044cad6b3f803fbb6143
vardan <vardanaggarwal@gmail.com>
2021-12-08 11:55:17 +0530

    categories

--- resources/views/categories/new.blade.php
+++ resources/views/categories/new.blade.php
@@ -5,25 +5,14 @@
         <form method="POST" action="/categories">
             @csrf
             <h1 class="font-bold text-2xl mb-4">
-              {{__('Add Category')}}
+                {{__('ui.new_f')}} {{__('ui.models.categories')}}
             </h1>
             <div>
                 <x-jet-label for="name" value="{{ __('Name') }}" />
                 <x-jet-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
             </div>
-
-            <div class="mt-4">
-                <x-jet-label for="description" value="{{ __('Description') }}" />
-                <textarea id="description" class="block mt-1 w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm" type="text-area" name="description"></textarea>
-            </div>
-            <div class="mt-4">
-                <x-jet-label for="links" value="{{ __('Link') }}" />
-                <x-jet-input id="links" class="block mt-1 w-full" type="text" name="links" :value="old('links')"  />
-            </div>
-
-
                 <x-jet-button class="mt-4 mx-auto">
-                    {{ __('Save') }}
+                    {{ __('Submit') }}
                 </x-jet-button>
             </div>
         </form>
