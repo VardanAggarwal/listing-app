bd6e2e71b0941235fa5447b299456b714c757686
vardan <vardanaggarwal@gmail.com>
2021-12-08 13:09:59 +0530

    category bug

--- app/Models/Reliable.php
+++ app/Models/Reliable.php
@@ -9,11 +9,13 @@ class Reliable extends MorphPivot
     protected $table = 'reliables';
     protected static function booted(){
         static::created(function($reliable){
-            $feed= new Feed;
-            $feed->feedable_id=$reliable->reliable_id;
-            $feed->feedable_type=$reliable->reliable_type;
-            $feed->resiliency_id=$reliable->resiliency_id;
-            $feed->save();
+            if($reliable->feedable_type!="App\\Models\\Category"){
+                $feed= new Feed;
+                $feed->feedable_id=$reliable->reliable_id;
+                $feed->feedable_type=$reliable->reliable_type;
+                $feed->resiliency_id=$reliable->resiliency_id;
+                $feed->save();
+            }
         });
     }
     //
