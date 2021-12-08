47a79c297820263836ce41d789bd30522c203b0c
vardan <vardanaggarwal@gmail.com>
2021-12-08 15:35:28 +0530

    feed fix

--- app/Models/Reliable.php
+++ app/Models/Reliable.php
@@ -10,11 +10,14 @@ class Reliable extends MorphPivot
     protected static function booted(){
         static::created(function($reliable){
             if($reliable->feedable_type!="App\\Models\\Category"){
-                $feed= new Feed;
-                $feed->feedable_id=$reliable->reliable_id;
-                $feed->feedable_type=$reliable->reliable_type;
-                $feed->resiliency_id=$reliable->resiliency_id;
-                $feed->save();
+                $found=Feed::where('feedable_id',$reliable->reliable_id)->where('feedable_type',$reliable->reliable_type)->count();
+                if(!$found>=1){
+                    $feed= new Feed;
+                    $feed->feedable_id=$reliable->reliable_id;
+                    $feed->feedable_type=$reliable->reliable_type;
+                    $feed->resiliency_id=$reliable->resiliency_id;
+                    $feed->save();
+                }
             }
         });
     }
