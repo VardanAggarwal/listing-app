ebdf7900ae7aa70fb8f80d625cddc698d009d5d2
vardan <vardanaggarwal@gmail.com>
2021-12-10 19:48:30 +0530

    resiliency-story bug

--- app/Models/Resiliency.php
+++ app/Models/Resiliency.php
@@ -10,7 +10,6 @@ class Resiliency extends Model
     use HasFactory;
     use Searchable;
     protected $guarded=[];
-    protected $with=['stories'];
 
     protected static function booted(){
         static::created(function($resiliency){
