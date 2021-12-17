8434e855f64a13daa9072ebcf31af1117d4258c8
vardan <vardanaggarwal@gmail.com>
2021-12-17 17:10:36 +0530

    profile card

---
+++ resources/views/components/profile-card.blade.php
@@ -0,0 +1,18 @@
+<div class="my-5 px-4 py-4 rounded-lg shadow border">
+  <div class="flex gap-5 items-center">
+      @if($model->user->profile_photo_url)
+      <div class="flex-shrink-0 justify-self-center">
+        <a href="/profiles/{{$model->id}}">
+          <img src="{{$model->user->profile_photo_url}}" class="rounded-full h-16 w-16" />
+        </a>
+      </div>
+      @endif
+      <div class="flex-auto">
+          <a href="/profiles/{{$model->id}}">
+            <span class="font-semibold text-lg text-green-900">{{$model->name}}</span><br>
+            <span class="">{{($model->address)}}, {{$model->pincode}}</span><br>
+            <span>{{$model->contact_number}}</span><br>
+        </a>
+      </div>
+  </div>
+</div>
