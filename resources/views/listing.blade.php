2578f113a82f6fbe60274920afd3e42b9a096778
vardan <vardanaggarwal@gmail.com>
2022-01-11 10:31:44 +0530

    listing page bug

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -18,7 +18,7 @@
     @if($listing->profile)
       @if($listing->profile->contact_number)
         <div class="mt-4">
-          <a href="/profiles/{{$listing->profile->id}}" class="underline"><span class="font-semibold text-md">{{__('ui.contact_for_services',['name'=>$listing->profile->user->name,'contact'=>$listing->profile->contact_number])}}</span></a>
+          <a href="/profiles/{{$listing->profile->id}}" class="underline"><span class="font-semibold text-md">{{__('ui.contact_for_services',['name'=>$listing->profile->name,'contact'=>$listing->profile->contact_number])}}</span></a>
         </div>
       @endif
       <x-profile-card :model="$listing->profile"></x-profile-card>
