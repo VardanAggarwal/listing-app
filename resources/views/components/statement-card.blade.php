3ae83017e890860cb57d32505381851c282ea187
vardan <vardanaggarwal@gmail.com>
2022-01-24 15:13:58 +0530

    statement card bug

--- resources/views/components/statement-card.blade.php
+++ resources/views/components/statement-card.blade.php
@@ -4,7 +4,7 @@
         @if($model->image)
             <a href="/statements/{{$model->id}}"><img src="{{$model->image}}" class="object-cover max-h-40 w-full sm:w-60 rounded mb-4 sm:m-0"></a>
         @endif
-        <a href="/statements/{{$model->id}}"><div class="prose overflow-ellipsis overflow-hidden max-h-40 sm:ml-4">{!!$model->statement!!}</div></a>
+        <a href="/statements/{{$model->id}}"><div class="overflow-ellipsis overflow-hidden max-h-12 sm:max-h-40 sm:ml-4">{{strip_tags($model->statement)}}</div></a>
     </div>
     @livewire('card-interests',['model'=>$model,'type'=>'Statement'], key('statement-'.$model->id))
 </div>
