7b28c308eb8d7274e57902ebd0d7a507ca23cb56
vardan <vardanaggarwal@gmail.com>
2022-01-28 00:05:48 +0530

    resiliencies in cards

--- resources/views/components/listing-card.blade.php
+++ resources/views/components/listing-card.blade.php
@@ -4,6 +4,11 @@
     <a href="/listings/{{$model->id}}"><span class="font-semibold text-xl text-green-900">{{$model->name}}</span></a>
     <span class="text-sm text-gray-500">{{__($model->type)}}</span>
   </div>
+  <div class="flex overflow-auto">
+    @foreach ($model->resiliencies as $resiliency)
+      <a href="/resiliencies/{{$resiliency->id}}" class="mr-4 whitespace-nowrap items-center py-1 px-2 bg-gray-800 rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:ring focus:ring-gray-300">{{$resiliency->name}}</a>
+    @endforeach
+  </div>
   <div class="flex gap-5 items-center">
       @if($model->image_url)
       <div class="flex-initial justify-self-center">
--- resources/views/components/statement-card.blade.php
+++ resources/views/components/statement-card.blade.php
@@ -18,7 +18,12 @@
             @if($model->image)
                 <a href="/statements/{{$model->id}}"><img src="{{$model->image}}" class="object-cover max-h-40 w-full sm:w-60 rounded mb-4 sm:m-0"></a>
             @endif
-            <a href="/statements/{{$model->id}}"><div class="overflow-ellipsis overflow-hidden max-h-12 sm:max-h-40 sm:ml-4">{{strip_tags($model->statement)}}</div></a>
+            <a href="/statements/{{$model->id}}"><div class="overflow-ellipsis overflow-hidden max-h-12 sm:max-h-40 sm:ml-4">{!!strip_tags($model->statement)!!}</div></a>
+        </div>
+        <div class="flex overflow-auto">
+          @foreach ($model->attached_resiliencies as $resiliency)
+            <a href="/resiliencies/{{$resiliency->id}}" class="mr-4 whitespace-nowrap items-center py-1 px-2 bg-gray-800 rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:ring focus:ring-gray-300">{{$resiliency->name}}</a>
+          @endforeach
         </div>
         @livewire('card-interests',['model'=>$model,'type'=>'Statement'], key($index.'statement-'.$model->id))
     @endif
