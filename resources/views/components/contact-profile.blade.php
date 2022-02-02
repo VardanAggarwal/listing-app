5749e939281e29f144885c075140d4cc81b9ffcc
vardan <vardanaggarwal@gmail.com>
2022-02-02 13:33:11 +0530

    checking whatsapp with new intent

--- resources/views/components/contact-profile.blade.php
+++ resources/views/components/contact-profile.blade.php
@@ -1,4 +1,4 @@
 <div class="flex gap-4 text-green-500">
     <a href="tel:{{$number}}" class="flex items-center gap-1"><i class="text-2xl fas fa-phone-alt"></i>{{__('Call')}}</a>
-    <a href="whatsapp://send/?phone={{Str::remove('+',$number)}}&text={{__('ui.contact_message')}}" class="ml-4 flex items-center gap-1"><i class="text-3xl fab fa-whatsapp"></i>{{__('Whatsapp')}}</a>
+    <a href="intent://send/?phone={{Str::remove('+',$number)}}&text={{__('ui.contact_message')}}#Intent;scheme=whatsapp;package=com.whatsapp;end" class="ml-4 flex items-center gap-1"><i class="text-3xl fab fa-whatsapp"></i>{{__('Whatsapp')}}</a>
 </div>
