4f6a5be6c31aae99f8f4bb2385de85d55727e9dd
vardan <vardanaggarwal@gmail.com>
2022-02-16 19:54:59 +0530

    expert icons

--- app/Http/Livewire/Expert/Show.php
+++ app/Http/Livewire/Expert/Show.php
@@ -8,6 +8,7 @@ class Show extends Component
 {
     public $profile;
     public $services;
+    public $service_types=['seed'=>'seedling','input'=>'basket-shopping','knowledge'=>'handshake-angle','training'=>'user-graduate','contract_farming'=>'sync','marketing'=>'money-bill-wave','others'=>'basket-shopping'];
     public function mount($profile){
         $this->services=$profile->expert_resiliencies->reduce(function($services,$item){
             return $services=array_unique(array_merge($services,$item->pivot->data['services']));
--- resources/views/livewire/expert/show.blade.php
+++ resources/views/livewire/expert/show.blade.php
@@ -10,9 +10,9 @@
   </span>
   <div class="mb-4 overflow-auto flex flex-nowrap gap-4">
     @foreach ($profile->expert_resiliencies as $resiliency)
-      <div class="flex-none border rounded-lg p-2">
+      <div class="flex flex-none border rounded-lg p-2 gap-2">
           @if($resiliency->image_url)
-              <img src="{{$resileincy->image_url}}" loading="lazy" class="h-8 w-8 rounded-full">
+              <img src="{{$resiliency->image_url}}" loading="lazy" class="h-8 w-8 rounded-full"/>
           @endif
           <span class="text-xl">{{$resiliency->name}}</span><br>
       </div>
@@ -21,7 +21,7 @@
     <span class="font-semibold text-lg">{{__('ui.expert.show.services')}}</span>
     <div class="mt-2 flex flex-wrap gap-2">
     @foreach($services as $service)
-      <span class="bg-gray-900 text-white rounded-lg px-2 py-1">{{__('ui.expert.services.'.$service)}}</span>
+      <span class="border rounded-lg px-2 py-1"><i class="fas fa-{{in_array($service,array_keys($service_types))?$service_types[$service]:$service_types['others']}}"></i> {{__(in_array($service,array_keys($service_types))?'ui.expert.services.'.$service:$service)}}</span>
     @endforeach
   </div>
                 
