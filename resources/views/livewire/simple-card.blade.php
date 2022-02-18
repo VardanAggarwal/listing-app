08bba8b39a130b449e5cb7ee2ec19428bbd3b9ca
vardan <vardanaggarwal@gmail.com>
2022-02-18 16:56:45 +0530

    simple card title

--- resources/views/livewire/simple-card.blade.php
+++ resources/views/livewire/simple-card.blade.php
@@ -2,12 +2,12 @@
     @if($image)
         <a href="{{$url}}" class="text-xl"><img src="{{$image}}" class="h-24 sm:h-36 mb-2 object-cover w-full" loading="lazy"/></a>
         @if($title)
-            <a href="{{$url}}" class="text-xl"><div class="max-h-12 overflow-hidden text-ellipsis text-xl">{{$title}}</div></a>
+            <a href="{{$url}}" class="text-xl"><div class="leading-6 max-h-12 overflow-hidden text-ellipsis text-xl">{{$title}}</div></a>
         @else
-            <a href="{{$url}}" class=""><div class="max-h-12 overflow-hidden text-ellipsis">{{$subtitle}}</div></a>    
+            <a href="{{$url}}" class=""><div class="leading-6 max-h-12 overflow-hidden text-ellipsis">{{$subtitle}}</div></a>    
         @endif
     @elseif($title)
-        <a href="{{$url}}" class="text-xl"><div class="max-h-12 overflow-hidden text-ellipsis text-xl mb-2">{{$title}}</div></a>
+        <a href="{{$url}}" class="text-xl"><div class="leading-6 max-h-12 overflow-hidden text-ellipsis text-xl mb-2">{{$title}}</div></a>
         <a href="{{$url}}" class=""><div class="max-h-24 overflow-hidden text-ellipsis">{{$subtitle}}</div></a>
     @else
         <a href="{{$url}}"><div class="max-h-36 overflow-hidden text-ellipsis text-xl">{{$subtitle}}</div></a>
