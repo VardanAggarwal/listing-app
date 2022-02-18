54c791a2e4e3efd6ea134b4c8d48120753a8102d
vardan <vardanaggarwal@gmail.com>
2022-02-18 17:17:16 +0530

    id in card clicked

--- resources/views/livewire/card-group.blade.php
+++ resources/views/livewire/card-group.blade.php
@@ -8,8 +8,9 @@
     @if($show)
         @php
             $parent=$model?str_replace("App\\Models\\","",get_class($model)):null;
+            $parent_id=$model?(isset($model->id)?$model->id:null):null
         @endphp
-        <div class="mb-6 rounded-lg shadow-md bg-gray-100 pt-4 pl-3" wire:loading.class="opacity-20" x-init="mixpanel.track('Card Group Shown',{'position':'{{$index}}','type':'{{$type}}','purpose':'{{$purpose}}','parent':'{{$model?$parent:null}}','parent_id':'{{$model?$model->id:null}}'})">
+        <div class="mb-6 rounded-lg shadow-md bg-gray-100 pt-4 pl-3" wire:loading.class="opacity-20" x-init="mixpanel.track('Card Group Shown',{'position':'{{$index}}','type':'{{$type}}','purpose':'{{$purpose}}','parent':'{{$model?$parent:null}}','parent_id':'{{$parent_id}}'})">
             <div class="flex justify-start gap-4">
                 @if($image)
                     <img src="{{$image}}" loading="lazy" class="h-8 w-8 rounded-full">
@@ -18,7 +19,7 @@
             </div>
             <div class="flex items-center overflow-auto gap-4 pr-5 py-4">
                 @forelse($feed as $item)
-                    <div class="flex-none {{$type=='Profile'?'w-64':'w-10/12 sm:w-96'}}" x-on:click="mixpanel.track('Card Clicked',{'position':'{{$loop->index}}','group_position':'{{$index}}','type':'{{$type}}','purpose':'{{$purpose}}','parent':'{{$model?$parent:null}}','parent_id':'{{$model?$model->id:null}}'})">
+                    <div class="flex-none {{$type=='Profile'?'w-64':'w-10/12 sm:w-96'}}" x-on:click="mixpanel.track('Card Clicked',{'position':'{{$loop->index}}','group_position':'{{$index}}','type':'{{$type}}','id':'{{$item->id}}','purpose':'{{$purpose}}','parent':'{{$model?$parent:null}}','parent_id':'{{$parent_id}}'})">
                         <div class="border p-4 bg-white shadow-md rounded-lg">
                             @switch($type)
                             @case('Statement')
