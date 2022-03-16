44c7ff891cf6d7c3a6124e7c87e4e8da82aec157
vardan <vardanaggarwal@gmail.com>
2022-03-16 13:43:22 +0530

    Order form

--- app/Http/Livewire/Leads/Form.php
+++ app/Http/Livewire/Leads/Form.php
@@ -3,13 +3,39 @@
 namespace App\Http\Livewire\Leads;
 
 use Livewire\Component;
-use App\Models\Resiliency;
+use Illuminate\Support\Facades\Auth;
 class Form extends Component
 {
-    public $resiliency_ids=[25,41,46,1,113,115,33,43,50,114,116];
+    public $resiliencies=[
+        ['id'=>25,'image_url'=>'https://listing-app.s3.ap-south-1.amazonaws.com/user/listing/QTTbHQaD2ZnvXLZm1ApqPodFmZUDmYbAo66HfzBJ.jpg','name'=>'वर्मी-कम्पोस्ट के लिए केंचुए'],
+        ['id'=>41,'image_url'=>'https://listing-app.s3.ap-south-1.amazonaws.com/user/listing/Rxl7RAPjaXog2Q5rA7ZjvRs7ZEdc5JACVbTPzTHg.jpg','name'=>'मशरुम के स्पॉन (बीज)'],
+        ['id'=>46,'image_url'=>'https://listing-app.s3.ap-south-1.amazonaws.com/user/resiliency/7hpyfUWFEYNJSVLVZCijfncAYaWTwAl5prB0YISp.jpg','name'=>'सब्जियों के बीज'],
+        ['id'=>1,'image_url'=>'https://app.seedsaversclub.com/storage/photos/R3kFCJkfnelKotxcl1VsbH5u3DeDtJTJ7eHHOndV.jpg','name'=>'औषधीय फसलों के बीज'],
+        ['id'=>113,'image_url'=>'https://listing-app.s3.ap-south-1.amazonaws.com/user/listing/v12EP0Ujajte8raYv3PjM0c1ge9n1cw6YNiG2YEQ.jpg','name'=>'डेयरी फार्मिंग के लिए पशु'],
+        ['id'=>115,'image_url'=>'https://listing-app.s3.ap-south-1.amazonaws.com/user/listing/MwW57hgyEd1fWcdBzB92rcj0fuMyzphvYCPty4mc.jpg','name'=>'बकरी'],
+        ['id'=>33,'image_url'=>'https://listing-app.s3.ap-south-1.amazonaws.com/user/resiliency/tcxNLPwGmwUuSPnSoSXBr8TQZAsq0MmzHfe0zjpu.jpg','name'=>'फलों के बीज एवं पौधे'],
+        ['id'=>43,'image_url'=>'https://listing-app.s3.ap-south-1.amazonaws.com/user/resiliency/H2Vtxa5lJRJxmvkOqzrd4YLYR0ldx8qBLIENttoE.jpg','name'=>'फूलों के बीज एवं पौधे'],
+        ['id'=>50,'image_url'=>'https://listing-app.s3.ap-south-1.amazonaws.com/user/listing/qj1awRxklDubXFqzwBKYzCOihHJuGaPod2g66G0M.jpg','name'=>'मधुमक्खी पेटी'],
+        ['id'=>114,'image_url'=>'https://listing-app.s3.ap-south-1.amazonaws.com/user/listing/jQsdBcH5A1lrorz5UyUCO7cYkI8RJlXZAqZ0puhD.jpg','name'=>'मुर्गी के चूजे'],
+        ['id'=>116,'image_url'=>'https://listing-app.s3.ap-south-1.amazonaws.com/user/listing/rCto38NLV3aSNkus0kpJ5hRyDOetenhBPz9O6jB8.jpg','name'=>'मछली के बीज/स्पॉन/जीरा']
+    ];
     public $selected=[];
     public $form=false;
     public $success=false;
+    public $profile;
+    public function mount(){
+        $user=Auth::user();
+        if($user){
+            if($user->profile){
+                $profile=$user->profile->contact_number?false:true;
+            }else{
+                $profile=true;
+            }
+        }else{
+            $profile=true;
+        }
+        $this->profile=$profile;
+    }
     public function submit(){
         $this->form=false;
         $this->success=true;
@@ -23,7 +49,7 @@ class Form extends Component
     }
     public function render()
     {
-        $resiliencies=Resiliency::findMany($this->resiliency_ids);
+        $resiliencies=collect($this->resiliencies);
         return view('livewire.leads.form',['resiliencies'=>$resiliencies])->layout('layouts.guest');
     }
 }
--- resources/views/livewire/leads/form.blade.php
+++ resources/views/livewire/leads/form.blade.php
@@ -35,21 +35,44 @@
     </div>
     @endunless
     @if($form)
-    <div>
+    <div class="px-4">
         <h1 class="text-xl">{{__('ui.leads.form.select_vc_title')}}</h1>
         <splan class="font-medium">{{__('ui.leads.form.select_vc_subtitle')}}</h1>
-        <div class="mb-4 overflow-auto flex flex-wrap gap-4">
+        <div class="mb-4 overflow-auto flex flex-wrap gap-4 py-4">
           @foreach ($resiliencies as $resiliency)
-            <div class="flex flex-none border rounded-lg p-2 gap-2 {{in_array($resiliency->id,$selected)?'bg-green-300':''}}" wire:click="toggleSelected({{$resiliency->id}})">
-                @if($resiliency->image_url)
-                    <img src="{{$resiliency->image_url}}" loading="lazy" class="h-8 w-8 rounded-full"/>
+            <div class="w-24 border grid justify-items-center rounded-lg {{in_array($resiliency['id'],$selected)?'bg-green-300':''}}" wire:click="toggleSelected({{$resiliency['id']}})">
+                @if($resiliency['image_url'])
+                    <img src="{{$resiliency['image_url']}}" loading="lazy" class="rounded-t-lg object-cover h-24 w-24"/>
                 @endif
-                <span class="text-xl">{{$resiliency->name}}</span><br>
+                <span class="text-sm m-2">{{$resiliency['name']}}</span>
             </div>
           @endforeach
         </div>
         <span class="text-xl">{{__('ui.leads.form.comments_label')}}</span>
         <textarea class="w-full rounded border-gray-400" oninput='this.style.height = "";this.style.height = this.scrollHeight + "px"' placeholder="{{__('ui.leads.form.comments_placeholder')}}"></textarea>
+        @if($profile)
+        <div>
+          <div>
+              <x-jet-label for="phone_number" value="{{ __('ui.contact_number') }}" />
+              <div class="flex items-center">
+                <span class="text-lg mr-2">+91</span>
+                <x-jet-input id="phone_number" class="block mt-1 w-full" wire:model="phone_number" type="text" name="phone_number" maxlength="10" required autofocus />
+              </div>
+          </div>
+          <div class="mt-4">
+              <x-jet-label for="name" value="{{ __('ui.name') }}" />
+              <x-jet-input id="name" class="block mt-1 w-full" type="text" name="name" wire:model="profile.name" autofocus/>
+          </div>
+          <div class="mt-4">
+              <x-jet-label for="address" value="{{ __('ui.address') }}" />
+              <x-jet-input id="address" class="block mt-1 w-full" type="text" name="address" wire:model="profile.address" autofocus/>
+          </div>
+          <div class="mt-4">
+              <x-jet-label for="pincode" value="{{ __('ui.pincode') }}" />
+              <x-jet-input id="pincode" class="block mt-1 w-full" type="text" name="pincode" wire:model="profile.pincode" autofocus/>
+          </div>
+        </div>
+        @endif
         <div class="mt-2 flex justify-center">
         <x-jet-button class="border rounded p-2 bg-gray-900 text-white" wire:click="submit">{{__('ui.leads.form.submit_button')}}</x-jet-button>
         </div>
