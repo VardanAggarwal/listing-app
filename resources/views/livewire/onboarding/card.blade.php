b259783777f8cbf8442e0fdbc079e617ad4336ce
vardan <vardanaggarwal@gmail.com>
2022-03-29 13:49:01 +0530

    onboarding card bug

--- resources/views/livewire/onboarding/card.blade.php
+++ resources/views/livewire/onboarding/card.blade.php
@@ -4,8 +4,8 @@
         <h1 class="text-2xl mb-4">{{__('ui.onboarding.card.title')}}</h1>
         <span class="font-medium">{{__('ui.onboarding.card.subtitle')}}</span>
         <div class="mt-2 flex justify-center gap-5">
-        <a href="onboarding"><x-jet-button class="border rounded-full p-2 bg-green-900 text-white text-md" x-on:click="mixpanel.track('Onboarding Card Clicked',{'position':'{{$index}}','title':'{{__('ui.onboarding.card.title')}}','subtitle':'{{__('ui.onboarding.card.subtitle')}}','button':'{{__('ui.onboarding.card.farmer_button')}}','role':'farmer'})">{{__('ui.onboarding.card.farmer_button')}}</x-jet-button></a>
-        <a href="expert/form"><x-jet-button class="border rounded-full p-2 bg-sky-900 text-white text-md" x-on:click="mixpanel.track('Onboarding Card Clicked',{'position':'{{$index}}','title':'{{__('ui.onboarding.card.title')}}','subtitle':'{{__('ui.onboarding.card.subtitle')}}','button':'{{__('ui.onboarding.card.expert_button')}}','role':'expert'})">{{__('ui.onboarding.card.expert_button')}}</x-jet-button></a>
+        <a href="/onboarding"><x-jet-button class="border rounded-full p-2 bg-green-900 text-white text-md" x-on:click="mixpanel.track('Onboarding Card Clicked',{'position':'{{$index}}','title':'{{__('ui.onboarding.card.title')}}','subtitle':'{{__('ui.onboarding.card.subtitle')}}','button':'{{__('ui.onboarding.card.farmer_button')}}','role':'farmer'})">{{__('ui.onboarding.card.farmer_button')}}</x-jet-button></a>
+        <a href="/expert/form"><x-jet-button class="border rounded-full p-2 bg-sky-900 text-white text-md" x-on:click="mixpanel.track('Onboarding Card Clicked',{'position':'{{$index}}','title':'{{__('ui.onboarding.card.title')}}','subtitle':'{{__('ui.onboarding.card.subtitle')}}','button':'{{__('ui.onboarding.card.expert_button')}}','role':'expert'})">{{__('ui.onboarding.card.expert_button')}}</x-jet-button></a>
         </div>
     </div>
     @endif
